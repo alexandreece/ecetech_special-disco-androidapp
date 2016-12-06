4a0ab9cd8d545c95fbc0b03893e787f53b025f3e
Alexandre <alexandre.suhas@edu.ecetech.fr>
2016-12-06 17:30:18 +0100

    ADD Models Equipe & Scores

--- app/src/main/java/SQLite/Equipe.java
+++ app/src/main/java/SQLite/Equipe.java
@@ -5,4 +5,46 @@ package SQLite;
  */
 
 public class Equipe {
+
+    private int idEquipe;
+    private String nomEquipe;
+    private int nbJoueurs;
+
+    public Equipe(String nomEquipe, int nbJoueurs) {
+        this.nomEquipe = nomEquipe;
+        this.nbJoueurs = nbJoueurs;
+    }
+
+    public int getIdEquipe() {
+        return idEquipe;
+    }
+
+    public void setIdEquipe(int idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    public String getNomEquipe() {
+        return nomEquipe;
+    }
+
+    public void setNomEquipe(String nomEquipe) {
+        this.nomEquipe = nomEquipe;
+    }
+
+    public int getNbJoueurs() {
+        return nbJoueurs;
+    }
+
+    public void setNbJoueurs(int nbJoueurs) {
+        this.nbJoueurs = nbJoueurs;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipe{" +
+                "idEquipe=" + idEquipe +
+                ", nomEquipe='" + nomEquipe + '\'' +
+                ", nbJoueurs=" + nbJoueurs +
+                '}';
+    }
 }
---
+++ app/src/main/java/SQLite/Scores.java
@@ -0,0 +1,72 @@
+package SQLite;
+
+/**
+ * Created by alex on 06/12/2016.
+ */
+
+public class Scores {
+
+    private int idScore;
+    private int score;
+    private String dateJeu;
+    private int niveauJeu;
+    private int Equipes_idEquipe;
+
+    public Scores(int score, String dateJeu, int niveauJeu, int equipes_idEquipe) {
+        this.score = score;
+        this.dateJeu = dateJeu;
+        this.niveauJeu = niveauJeu;
+        Equipes_idEquipe = equipes_idEquipe;
+    }
+
+    public int getIdScore() {
+        return idScore;
+    }
+
+    public void setIdScore(int idScore) {
+        this.idScore = idScore;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getDateJeu() {
+        return dateJeu;
+    }
+
+    public void setDateJeu(String dateJeu) {
+        this.dateJeu = dateJeu;
+    }
+
+    public int getNiveauJeu() {
+        return niveauJeu;
+    }
+
+    public void setNiveauJeu(int niveauJeu) {
+        this.niveauJeu = niveauJeu;
+    }
+
+    public int getEquipes_idEquipe() {
+        return Equipes_idEquipe;
+    }
+
+    public void setEquipes_idEquipe(int equipes_idEquipe) {
+        Equipes_idEquipe = equipes_idEquipe;
+    }
+
+    @Override
+    public String toString() {
+        return "Scores{" +
+                "idScore=" + idScore +
+                ", score=" + score +
+                ", dateJeu='" + dateJeu + '\'' +
+                ", niveauJeu=" + niveauJeu +
+                ", Equipes_idEquipe=" + Equipes_idEquipe +
+                '}';
+    }
+}
