ff624340a9e9e1f08d3cec2dc1d9c6038f549716
Alexandre <alexandre.suhas@edu.ecetech.fr>
2017-01-10 15:55:59 +0100

    Essai SelectWord

--- app/src/main/java/com/example/lama/lamapp/SelectWordDictionaire.java
+++ app/src/main/java/com/example/lama/lamapp/SelectWordDictionaire.java
@@ -1,17 +1,27 @@
 package com.example.lama.lamapp;
 
-import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SelectWordDictionaire extends AppCompatActivity {
 
     ListView CatList;
+    SQLiteDatabase db = SQLiteDatabase.openDatabase("bdd/LAMapp.db",null,SQLiteDatabase.CONFLICT_NONE);
+    ArrayList<String> mots = new ArrayList<String>();
 
-    SQLiteDatabase db = SQLiteDatabase.openDatabase("",null,SQLiteDatabase.CONFLICT_NONE);
+    void dbAction(){
+        Cursor resultSet = db.rawQuery("select * from WordsList order by category",null);
+        resultSet.moveToFirst();
+        if (resultSet.moveToNext()){
+            mots.add(resultSet.getString(3));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,8 @@ public class SelectWordDictionaire extends AppCompatActivity {
         setContentView(R.layout.activity_select_word_dictionaire);
 
         CatList = (ListView) findViewById(R.id.CatList);
+        dbAction();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectWordDictionaire.this,android.R.layout.simple_list_item_1,mots);
+        CatList.setAdapter(adapter);
     }
 }
