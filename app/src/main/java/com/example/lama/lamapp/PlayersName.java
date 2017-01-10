c72799781b09e03140a6be4db97ac3b4a92d4525
Maeva <maeva.mgt@hotmail.fr>
2017-01-10 17:39:35 +0100

    Players name

---
+++ app/src/main/java/com/example/lama/lamapp/PlayersName.java
@@ -0,0 +1,13 @@
+package com.example.lama.lamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayersName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_name);
+    }
+}
