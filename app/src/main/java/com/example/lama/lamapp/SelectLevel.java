b4b10ed178e3d20da72d00719d52d4ec70b1c9a4
FIREBEAR\Lucas <lucas.randria@hotmail.fr>
2017-01-16 22:28:45 +0100

    RENAME SelectActivity to SelectLevel

--- app/src/main/java/com/example/lama/lamapp/SelectLevel.java
+++ app/src/main/java/com/example/lama/lamapp/SelectLevel.java
@@ -3,7 +3,7 @@ package com.example.lama.lamapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SelectActivity extends AppCompatActivity {
+public class SelectLevel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
