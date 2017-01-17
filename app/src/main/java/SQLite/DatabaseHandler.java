122760ba0a53c2f97aadd9379d99923d83d84ac9
Alexandre <alexandre.suhas@edu.ecetech.fr>
2017-01-17 01:07:25 +0100

    Clean Up pour prezzzzzzz

--- app/src/main/java/SQLite/DatabaseHandler.java
+++ app/src/main/java/SQLite/DatabaseHandler.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1; //Database Version
-    private static final String DATABASE_NAME = "LAMapp"; //Database Name
+    private static final String DATABASE_NAME = "LAMapp.db"; //Database Name
 
     //★★★★★★★★★★★★★★★★★★★★★★★★★★★★
 
