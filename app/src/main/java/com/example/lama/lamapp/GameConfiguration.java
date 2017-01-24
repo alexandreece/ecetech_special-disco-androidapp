e12b6233615b4c4bde74815c8d60211c652cd356
Maeva <maeva.mgt@hotmail.fr>
2017-01-24 16:36:55 +0100

    Menu Jeu (manque icone)

--- app/src/main/java/com/example/lama/lamapp/GameConfiguration.java
+++ app/src/main/java/com/example/lama/lamapp/GameConfiguration.java
@@ -2,7 +2,6 @@ package com.example.lama.lamapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
 
 public class GameConfiguration extends AppCompatActivity {
---
+++ app/src/main/java/com/example/lama/lamapp/MenuJeu.java
@@ -0,0 +1,15 @@
+package com.example.lama.lamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuJeu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_jeu);
+    }
+
+
+}
