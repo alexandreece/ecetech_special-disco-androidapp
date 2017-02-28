edc522b2116d020c40da9d6c84a7b93d2a4a82ee
Adrien <adrien.duhoux@gmail.com>
2017-02-28 15:33:24 +0100

    suite

--- app/src/main/java/com/example/lama/lamapp/PlayersName.java
+++ app/src/main/java/com/example/lama/lamapp/PlayersName.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
@@ -34,19 +33,21 @@ public class PlayersName extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players_name);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        Intent intent = getIntent();
 
+
+        //Getting the game class from previous view
+        Intent intent = getIntent();
         game = (Game) intent.getSerializableExtra("game");
 
+        //create object editext, button
+        valider = (Button) findViewById(R.id.playerName_Button_okPlayerName);
+        valider.setOnClickListener(this);
+        teamA = (EditText) findViewById(R.id.playerName_Editext_nameEqA);
+        teamB = (EditText) findViewById(R.id.playerName_Editext_nameEqB);
+
+        //generating Arraylist for each team and adding adapter
         equipeA = new ArrayList<>();
         equipeB = new ArrayList<>();
-
-        valider = (Button) findViewById(R.id.okPlayerName);
-        teamA = (EditText) findViewById(R.id.nameEqA);
-        teamB = (EditText) findViewById(R.id.nameEqB);
-
-        valider.setOnClickListener(this);
-
 
        for(int i =0; i<game.getNbPlayers();i++){
             Joueur joueurA = new Joueur();
@@ -56,8 +57,9 @@ public class PlayersName extends Activity implements OnClickListener {
             equipeB.add(joueurB);
         }
 
-        ListeA = (ListView) findViewById(R.id.listA);
-        ListeB = (ListView) findViewById(R.id.listB);
+        ListeA = (ListView) findViewById(R.id.playerName_ListView_listA);
+        ListeB = (ListView) findViewById(R.id.playerName_Listview_listB);
+
 
         adapterA = new PlayerNameAdapter(this, R.layout.layout_playername,equipeA);
         adapterB = new PlayerNameAdapter(this, R.layout.layout_playername,equipeB);
@@ -68,19 +70,19 @@ public class PlayersName extends Activity implements OnClickListener {
 
     }
 
+    // action button "valider"
     @Override
     public void onClick(View v) {
+        //team name
         game.setNameTeamA(teamA.getText().toString());
         game.setNameTeamB(teamB.getText().toString());
+
+        //team players
         game.addTeamA(adapterA.getList());
         game.addTeamB(adapterB.getList());
-        Log.i("Team A", game.getTeamA_List_Joueurs().toString());
-        Log.i("Team B", game.getTeamB_List_Joueurs().toString());
 
+        //sendind game object to next class
         Intent intent_next = new Intent(PlayersName.this, EnterWord.class);
-
-        Log.i("Team Name", "A: " + game.getNameTeamA());
-        Log.i("Team Name", "B: " + game.getNameTeamB());
         intent_next.putExtra("game", game);
         startActivity(intent_next);
 
