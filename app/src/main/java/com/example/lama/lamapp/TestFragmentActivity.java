53cffb91a6de3b6f608d3d10896f7647bf94591e
VRSTL <lucas.randria@hotm>
2017-03-01 03:25:10 +0100

    EDIT test activities

--- app/src/main/java/com/example/lama/lamapp/TestFragmentActivity.java
+++ app/src/main/java/com/example/lama/lamapp/TestFragmentActivity.java
@@ -1,5 +1,6 @@
 package com.example.lama.lamapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,20 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.lama.lamapp.DAOs.Word;
-
-import java.util.List;
-
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
-
 public class TestFragmentActivity extends AppCompatActivity {
-
-    String[] WordList = {"Chien", "Chat", "Poisson", "Ours", "Lombric", "Loup", "Chèvre", "Hyène", "Pigeon", "Aigle", "Souris", "Rat", "Grenouille", "Vache"};
-   /* DatabaseHandler db = new DatabaseHandler(this);
-    List<Word> WordList = db.getWordsList();*/
-//    db.close();
-
-    private Handler mHandler = new Handler();
 
     TextView mTimer;
     TextView mWord;
@@ -33,29 +21,31 @@ public class TestFragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fragment);
 
+        Intent intent = getIntent();
+        Game game = (Game) intent.getSerializableExtra("game");
+
         mWord = (TextView) findViewById(R.id.ShowWord);
-        mWord.setText(WordList[0]);
+        mWord.setText(game.getWord(0));
 
         mCount = (TextView) findViewById(R.id.ShowCount);
-        CountText = "0/" + WordList.length;
+        CountText = "0/" + game.Words_List.size();
         mCount.setText(CountText);
 
         // START TIMER
-        startTimerThread();
+        startTimerThread(game);
 
         // WORD
-        WordListThread();
-
-        //String word = WordList.get(0).getWord();
+        WordListThread(game);
     }
 
     // TIMER FUNCTION
-    void startTimerThread() {
+    void startTimerThread(final Game game) {
         final Handler handler = new Handler();
         Runnable runnable = new Runnable() {
             public void run() {
-                int timer = 60;
+                int timer = 61;
                 while (timer != 0) {
+                    timer--;
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -68,41 +58,52 @@ public class TestFragmentActivity extends AppCompatActivity {
                             mTimer.setText(finalTimer);
                         }
                     });
-                    timer--;
                 }
+                Intent intent_next = new Intent(TestFragmentActivity.this, Test.class);
+                intent_next.putExtra("game", game);
+                startActivity(intent_next);
             }
         };
         new Thread(runnable).start();
     }
 
-    void WordListThread (){
+    void WordListThread (final Game game){
         new Thread(new Runnable() {
             public void run() {
                 final Button button_validate = (Button) findViewById(R.id.button_valid_word);
                 button_validate.setOnClickListener(new View.OnClickListener(){
                     int i = 0;
                     int Count = 0;
-                    int NbWord = WordList.length;
+                    int CurrentPoint = 0;
+                    int NbWord = game.Words_List.size(); //game.getNbWords();
                     public void onClick(View v){
                         if(i < NbWord) {
                             i++;
-                            mWord.setText(WordList[i]);
+                            CurrentPoint++;
+                            mWord.setText(game.getWord(i));
                             Count++;
-                            CountText = Integer.toString(Count)+ "/" + Integer.toString(WordList.length);
+                            CountText = Integer.toString(Count)+ "/" + Integer.toString(NbWord);
                             mCount.setText(CountText);
-                            if(i == WordList.length - 1) i = 0;
+                            game.setNbPointsTurn(CurrentPoint);
+                            Log.i("Test", "NbPoint = " + game.getNbPointsTurn());
+                            if(i == NbWord - 1) i = 0;
+                        }
+                        if(Count == game.Words_List.size()){
+                            Intent intent_next = new Intent(TestFragmentActivity.this, Test.class);
+                            intent_next.putExtra("game", game);
+                            startActivity(intent_next);
                         }
                     }
                 });
                 final Button button_quit = (Button) findViewById(R.id.button_quit_word);
                 button_quit.setOnClickListener(new View.OnClickListener(){
                     int i = 0;
-                    int NbWord = WordList.length;
+                    int NbWord = game.Words_List.size();
                     public void onClick(View v){
                         if(i < NbWord) {
                             i++;
-                            mWord.setText(WordList[i]);
-                            if(i == WordList.length - 1) i = 0;
+                            mWord.setText(game.getWord(i));
+                            if(i == NbWord - 1) i = 0;
                         }
                     }
                 });
