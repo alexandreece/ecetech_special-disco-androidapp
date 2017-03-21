9d8d0aca6972b4b531942b6208bcbf570d413289
Maeva <maeva.mgt@hotmail.fr>
2017-03-21 16:34:55 +0100

    enterwords 2

--- app/src/main/java/com/example/lama/lamapp/EnterWord2.java
+++ app/src/main/java/com/example/lama/lamapp/EnterWord2.java
@@ -3,6 +3,7 @@ package com.example.lama.lamapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.lama.lamapp.DAOs.Joueur;
 
@@ -13,13 +14,18 @@ import java.util.Objects;
 public class EnterWord2 extends AppCompatActivity {
 
     public Game game;
+    public ArrayList<Object> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter_word2);
-
         Intent intent = getIntent();
         game = (Game) intent.getSerializableExtra("game");
+        setContentView(R.layout.activity_enter_word2);
+        ListView listView = (ListView)findViewById(R.id.ActivityEnterword2_listView);
+        list = mergeList(game.getTeamA_List_Joueurs(),game.getTeamB_List_Joueurs(),game.TeamA,game.TeamB);
+
+
+        listView.setAdapter(new EnterWordAdapter2(this, list));
     }
 
 
--- app/src/main/java/com/example/lama/lamapp/EnterWordAdapter2.java
+++ app/src/main/java/com/example/lama/lamapp/EnterWordAdapter2.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.lama.lamapp.DAOs.Joueur;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
  * Created by Mae on 21/03/2017.
  */
 
-public class EnterWordAdapter2 {
+public class EnterWordAdapter2 extends BaseAdapter {
     private ArrayList<Object> list;
     private LayoutInflater inflater;
     private static final int ROW = 0;
@@ -24,6 +25,16 @@ public class EnterWordAdapter2 {
     public EnterWordAdapter2(Context context, ArrayList<Object> list) {
         this.list = list;
         this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
     }
 
     public long getItemId(int position) {
@@ -50,7 +61,7 @@ public class EnterWordAdapter2 {
     public boolean isEnabled(int position) {
         return (getItemViewType(position) == ROW);
     }
-    
+
     public View getView(int position, View convertView, ViewGroup parent) {
         int type = getItemViewType(position);
         if (convertView == null) {
