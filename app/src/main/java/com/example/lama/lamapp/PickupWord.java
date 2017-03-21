aaef7b049ed0b02ae9049822fed23b84e4da6d52
Adrien <adrien.duhoux@gmail.com>
2017-03-21 17:44:06 +0100

    Player word

--- app/src/main/java/com/example/lama/lamapp/PickupWord.java
+++ app/src/main/java/com/example/lama/lamapp/PickupWord.java
@@ -1,13 +1,39 @@
 package com.example.lama.lamapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.lama.lamapp.DAOs.Joueur;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class PickupWord extends AppCompatActivity {
+
+    public Game game;
+    public ArrayList<Object> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //import game
+        Intent intent = getIntent();
+        game = (Game) intent.getSerializableExtra("game");
+
+        //set content view
         setContentView(R.layout.activity_pickup_word);
+        ListView listView = (ListView)findViewById(R.id.Activity_pickupword_listview_list);
+
+        //remplissage list
+        for (int i = 0; i < game.getNbWords(); i++) {
+            String string = "";
+            list.add(string);
+
+        }
+
+        listView.setAdapter(new EnterWordAdapter2(this, list));
     }
+
 }
---
+++ app/src/main/java/com/example/lama/lamapp/PickupWordAdapter.java
@@ -0,0 +1,140 @@
+package com.example.lama.lamapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.lama.lamapp.DAOs.Word;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by adri-laptop on 21/03/17.
+ */
+
+public class PickupWordAdapter extends ArrayAdapter<String> implements View.OnClickListener {
+
+    /*
+    Variables
+     */
+    private Context context;
+    private LayoutInflater inflater;
+    private ArrayList<String> list;
+    private Game game;
+
+
+    /*
+    Constructeur
+     */
+
+    public PickupWordAdapter(Context context, int resourceId, ArrayList<String> myItems, Game game) {
+
+        super(context, resourceId, myItems);
+
+        this.context = context;
+        inflater = LayoutInflater.from(this.context);
+        list = myItems;
+        this.game = game;
+    }
+
+    /*
+    getview
+     */
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //String mot = getItem(position);
+        convertView = inflater.inflate(position,null);
+
+        //Creating Button
+        Button precedent = (Button) convertView.findViewById(R.id.Layout_PickupWord_Button_Pastword);
+        Button dictionnaire = (Button) convertView.findViewById(R.id.Layout_PickupWord_Button_Dicoword);
+        Button random = (Button) convertView.findViewById(R.id.Layout_PickupWord_Button_Randomword);
+        Button ok = (Button) convertView.findViewById(R.id.Layout_PickupWord_Button_ok);
+
+        //Adding listener on buttons
+        precedent.setOnClickListener(this);
+        dictionnaire.setOnClickListener(this);
+        random.setOnClickListener(this);
+        ok.setOnClickListener(this);
+
+        //adding Tag to recognize button
+        precedent.setTag(position);
+        dictionnaire.setTag(position);
+        random.setTag(position);
+        ok.setTag(position);
+
+        return convertView;
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        Intent activity;
+
+        //Show the wordlist of previous word
+        if (v.getId() == R.id.Layout_PickupWord_Button_Pastword) {
+            activity = new Intent(this.getContext(), SelectWord.class);
+            activity.putExtra("position", (int) v.findViewById(R.id.Layout_PickupWord_Button_Pastword).getTag());
+
+            activity.putExtra("game", game);
+            activity.putExtra("ab", ab);
+            this.getContext().startActivity(activity);
+        }
+
+        //show the dictionary with words by category
+        else if (v.getId() == R.id.Layout_PickupWord_Button_Dicoword) {
+            activity = new Intent(this.getContext(), SelectRandomWord.class);
+            activity.putExtra("position", (int) v.findViewById(R.id.Layout_PickupWord_Button_Dicoword).getTag());
+
+            activity.putExtra("game", game);
+            activity.putExtra("ab", ab);
+            this.getContext().startActivity(activity);
+
+        }
+
+        //Select a random word
+        else if (v.getId() == R.id.Layout_PickupWord_Button_Randomword) {
+            Random alea = new Random();
+            DatabaseHandler db = new DatabaseHandler(this.getContext());
+            List<Word> wordsR = db.getWordsList();
+            db.close();
+            String ranMot = wordsR.get(alea.nextInt(wordsR.size())).getWord();
+
+            Log.i("pos", "onClick: " + (int) v.findViewById(R.id.Layout_PickupWord_Button_Randomword).getTag());
+            list.set((int) v.findViewById(R.id.Layout_PickupWord_Button_Randomword).getTag(), ranMot);
+            this.notifyDataSetChanged();
+
+        }
+        // ok button
+        else if (v.getId() == R.id.Layout_PickupWord_Button_ok) {
+            if (!list.isEmpty()) {
+                Log.i("IF §§§", "EnterWordAdapter: ");
+                game.getWords_List().add(list.get(R.id.Layout_PickupWord_Button_ok));
+                list.remove(R.id.Layout_PickupWord_Button_ok);
+            }else{
+                Log.i("ELSE §§§ ", "EnterWordAdapter: ");
+
+            }
+                this.notifyDataSetChanged();
+
+        }
+
+
+    }
+
+}
+
+
+
