9aede856ab21cdc38a2f68262759bde73ceb022d
Maeva <maeva.mgt@hotmail.fr>
2017-03-21 15:27:09 +0100

    enterwordadapter 2

--- app/src/main/java/com/example/lama/lamapp/DAOs/EnterWordAdapterV2.java
+++
@@ -1,8 +0,0 @@
-package com.example.lama.lamapp.DAOs;
-
-/**
- * Created by adri-laptop on 21/03/17.
- */
-
-public class EnterWordAdapterV2 {
-}
---
+++ app/src/main/java/com/example/lama/lamapp/EnterWordAdapter2.java
@@ -0,0 +1,8 @@
+package com.example.lama.lamapp;
+
+/**
+ * Created by Mae on 21/03/2017.
+ */
+
+public class EnterWordAdapter2 {
+}
