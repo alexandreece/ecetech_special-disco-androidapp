888d9fae9ae6714a4492783a2e9b532e7da8bc72
Adrien <adrien.duhoux@gmail.com>
2017-03-21 14:49:45 +0100

    EnterWord V2

---
+++ app/src/main/java/com/example/lama/lamapp/DAOs/EnterWordAdapterV2.java
@@ -0,0 +1,8 @@
+package com.example.lama.lamapp.DAOs;
+
+/**
+ * Created by adri-laptop on 21/03/17.
+ */
+
+public class EnterWordAdapterV2 {
+}
---
+++ app/src/main/java/com/example/lama/lamapp/EnterWordV2.java
@@ -0,0 +1,13 @@
+package com.example.lama.lamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterWordV2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_word_v2);
+    }
+}
