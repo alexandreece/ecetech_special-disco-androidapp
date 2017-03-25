c6b0a876a7286d1c53ae8ade8cb6425294e436a1
Maeva <maeva.mgt@hotmail.fr>
2017-03-25 20:45:03 +0100

    enterwords 2 OK

--- app/src/main/java/com/example/lama/lamapp/EnterWord2.java
+++ app/src/main/java/com/example/lama/lamapp/EnterWord2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.lama.lamapp.DAOs.Joueur;
 
