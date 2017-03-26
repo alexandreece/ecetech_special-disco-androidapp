a11944fafdbc1f6372a4309340494212625db4a0
Unknown <adrien.duhoux@gmail.com>
2017-03-26 19:22:50 +0200

    Shared preferences reset

--- app/src/main/java/com/example/lama/lamapp/MenuJeu.java
+++ app/src/main/java/com/example/lama/lamapp/MenuJeu.java
@@ -2,6 +2,7 @@ package com.example.lama.lamapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,10 @@ public class MenuJeu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_jeu);
+        SharedPreferences sharedPreferences = getSharedPreferences("NBWord", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
     }
 
     public void goto_SelectLevel(View view) {
