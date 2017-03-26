01261460a4cbb885919ce726873e927593d6dabb
VRSTL <lucas.randria@hotm>
2017-03-26 11:55:22 +0200

    ADD Dynamic point display ADD Random point distribution

--- app/src/main/java/com/example/lama/lamapp/EndTurn.java
+++ app/src/main/java/com/example/lama/lamapp/EndTurn.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.RelativeLayout;
+import java.util.Random;
 
 public class EndTurn extends AppCompatActivity {
 
@@ -82,6 +84,52 @@ public class EndTurn extends AppCompatActivity {
         String PointsMessage = "L'équipe adverse reçoit " + CurrentPoints +  " LAMAs";
         mPointsMessage = (TextView) findViewById(R.id.Points_Message);
         mPointsMessage.setText(PointsMessage);
+
+        int[] TablePoint = new int[game.getNbPlayers()];
+
+        for(int i = 0; i < game.getNbPlayers(); i++){
+            TablePoint[i] = 0;
+        }
+
+        for(int i = 0; i < game.getCount(); i++){
+            Random rand = new Random();
+            int player = rand.nextInt(game.getNbPlayers());
+
+            TablePoint[player] = TablePoint[player] + 1;
+        }
+
+        for(int i = 0; i < game.getNbPlayers(); i++){
+            Log.i("Table","Point = " + TablePoint[i]);
+        }
+
+        final TextView[] myTextViews = new TextView[game.NbPlayers];
+
+        RelativeLayout activity_end_turn = (RelativeLayout)findViewById(R.id.activity_end_turn);
+        Log.i("Nb","Players =" + game.getNbPlayers());
+        int n = 450;
+        for (int i = 0; i < game.getNbPlayers(); i++) { // game.getNbPlayers()
+            final TextView rowTextView = new TextView(this);
+            Log.i("val", " i =" + i );
+
+            if (PlayerToPlay[0]  == 0){
+                rowTextView.setText(game.getNameJoueurTeamB(i)+ " ⟶ " + TablePoint[i] + " lamas");
+            }else if (PlayerToPlay[0]  == 1){
+                rowTextView.setText(game.getNameJoueurTeamA(i)+ " ⟶ " + TablePoint[i] + " lamas");
+            }
+
+            RelativeLayout relativeLayout = new RelativeLayout(this);
+            relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT));
+            RelativeLayout.LayoutParams relativeParams = (RelativeLayout.LayoutParams)relativeLayout.getLayoutParams();
+            relativeParams.setMargins(500, n, 10, 10);
+            rowTextView.setLayoutParams(relativeParams);
+            rowTextView.setTextSize(25);
+            rowTextView.setBackgroundColor(0);
+            activity_end_turn.addView(rowTextView);
+
+            myTextViews[i] = rowTextView;
+
+            n = n + 100;
+        }
     }
 
     public void goto_NextPlayer(View view) {
